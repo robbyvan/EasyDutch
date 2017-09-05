06d0898721b839e12802d39df68c099f98c5a9e0
Robby <fzpinus@gmail.com>
2017-09-05 15:16:56 -0700

    fix(timeout): fix request timeout behavior

--- app/utils/Request.js
+++ app/utils/Request.js
@@ -4,7 +4,7 @@ import Config from './Config';
 
 const request = {};
 
-request.get = (api, params) => new Promise(async(resolve, rejcet) => {
+request.get = (api, params) => new Promise(async (resolve, reject) => {
   let completeUrl = Config.server + api;
   if (params) {
     completeUrl = `${completeUrl}?${queryString.stringify(params)}`
@@ -20,37 +20,34 @@ request.get = (api, params) => new Promise(async(resolve, rejcet) => {
     myHeaders = new Headers({
       'Accept': 'application/json',
       'Content-Type': 'application/json',
-      // 'Cube-Authorization': currentUser.token,
     });
   }else {
     reject({msg: '当前未登录'});
   }
-
   const options = {
     method: 'GET',
     headers: myHeaders,
   };
 
   //开始计时, 20秒内无response视为无法连接
-  const requestTimer = setTimeout(() => {
-    reject({success: false, msg: '暂时无法连接到服务器, 稍后再试试吧'});
-  }, 20000); 
+  const requestTimer = setTimeout(function() {
+    reject("Can't connect to server now, try it again later.");
+  }, 20000);
 
-  resolve(
-    fetch(completeUrl, options)
-      .then((res) => {
-        clearTimeout(requestTimer);
-        return res.json();
-      })
-      .then((responseJson) => responseJson)
-      .catch((error) => {
-          console.error(error);
-      })
-  );
+  // resolve(
+  fetch(completeUrl, options)
+    .then((res) => {
+      clearTimeout(requestTimer);
+      return res.json();
+    })
+    .then((responseJson) => resolve(responseJson))
+    .catch((error) => {
+      console.error(error);
+    })
+  // );
 });
 
 request.login = (api, body) => new Promise((resolve, reject) => {
-  // const currentUser = getUser();
   const completeUrl = Config.server + api;
 
   const myHeaders = new Headers({
@@ -65,8 +62,9 @@ request.login = (api, body) => new Promise((resolve, reject) => {
   };
   console.log('POST to: ', completeUrl, options);
 
-  const requestTimer = setTimeout(() => {
-    reject({ success: 'false', msg: 'Can\'t connect to server now, try it again later.' });
+  // const that = this;
+  const requestTimer = setTimeout(function(){
+    reject({ success: false, msg: 'Can\'t connect to server now, try it again later.' });
   }, 20000);
 
   resolve(
