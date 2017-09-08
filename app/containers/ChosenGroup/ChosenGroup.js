1c8e50be520bda6cee50656646afa56f3a26889c
Robby <fzpinus@gmail.com>
2017-09-08 06:56:28 -0700

    fix: delete some uesless code

--- app/containers/ChosenGroup/ChosenGroup.js
+++ app/containers/ChosenGroup/ChosenGroup.js
@@ -107,7 +107,7 @@ class Group extends Component {
     );
     return (
       <ListItem
-        // onPress={() => navigation.navigate('Ha', { groupID: item.groupID, name: item.name })}
+        onPress={() => null}
         key={item}
         title={rowContent}
         leftIcon={textAvatar}
@@ -145,61 +145,14 @@ class Group extends Component {
       <View
         style={style.container}
       >
-
-        
-        
         <View style={{flex: 1}}>
           {status}
-          {null && <View style={style.title}>
-                      <Text style={style.titleText}>Transactions</Text>
-                    </View>}
           <FlatList
             data={state.chosenGroup.members}
             keyExtractor={item => item}
             renderItem={this.renderItem}
           />
         </View>
-
-        <Modal
-          visible={false}
-          onRequstClose={() => null}
-        >
-          <View style={{flex: 1}}>
-            <View style={style.title}>
-              <Text style={style.titleText}>Brief</Text>
-            </View>
-              {state.myBill &&
-                <List style={{margin: 0, backgroundColor: '#fff'}}>
-                  <ListItem
-                    onPress={() => Alert.alert('haha')}
-                    leftIcon={<EvilIcons name="arrow-right" size={40} color={custom.receiveColor} style={{alignSelf: 'center'}} />}
-                    title={
-                      <View style={style.rowContainer}>
-                        <Text style={[style.rowLabel, { color: custom.receiveColor }]}>Receive</Text>
-                        <Text style={style.rowValue}>${state.myBill.totalReceive  || '0'}</Text>
-                      </View>
-                    }
-                    hideChevron
-                  />
-                  <ListItem
-                    onPress={() => Alert.alert('haha')}
-                    leftIcon={<EvilIcons name="arrow-left" size={40} color={custom.payColor} style={{alignSelf: 'center'}}/>}
-                    title={
-                      <View style={style.rowContainer}>
-                        <Text style={[style.rowLabel, { color: custom.payColor }]}>Pay</Text>
-                        <Text style={style.rowValue}>${state.myBill.totalPay || '0'}</Text>
-                      </View>
-                    }
-                    hideChevron
-                  />
-                </List>
-              }
-          </View>  
-        </Modal>
-        
-        
-        
-
       </View>
     );
   }
